129806846663fe69e6a018b8f259b64c8dba3c86
Gustav Karreskog <gustav.karreskog@gmail.com>
2018-10-02 02:10:34 -0400

    Small fixes, seems to be somwthing buggy happening when ran from terminal

--- julia/main_linear_DT.jl
+++ julia/main_linear_DT.jl
@@ -10,14 +10,14 @@ addprocs(Sys.CPU_THREADS)
 @everywhere push!(LOAD_PATH, pwd())
 @everywhere using LinearDT
 
-
+# %%
 # All theses values have to be shared among the workers
 @everywhere begin
     pop_size = 100
     max_t = 3
     p_extend = 0.7
     n_elit = 2
-    periods = 20
+    periods = 5
     n_problems = 1000
     sigmas = [1., 0.7, 0.2, 0.1]
     selection_mechanism = "tournament"
@@ -25,14 +25,17 @@ addprocs(Sys.CPU_THREADS)
 end
 
 # The globals are needed to work from terminal, bugish behavior
+# global pop = [init_tree(max_t, p_extend, sigmas) for i in 1:pop_size]
+# global prev_best = deepcopy(pop[1])
 global pop = [init_tree(max_t, p_extend, sigmas) for i in 1:pop_size]
-global prev_best = deepcopy(pop[1])
+global prev_best = pop[1]
 
 
 for i in 1:periods
     # The globals are needed to work from terminal, bugish behavior
     global pop = pop
     global prev_best = prev_best
+    println(prev_best in pop)
     x_vec = gen_investment_list(sigmas, n_problems)
     @everywhere begin
         x_vec = $x_vec
@@ -56,7 +59,8 @@ for i in 1:periods
         weights = Float64[]
     end
     elit_pop = [x.tree for x in perf[1:n_elit]]
-    global prev_best = deepcopy(elit_pop[1])
+    # global prev_best = elit_pop[1]
+    prev_best = elit_pop[1]
     @everywhere begin
         weights = $weights
         perf = $perf
@@ -65,7 +69,8 @@ for i in 1:periods
     new_pop = pmap(gen_child_parallel, 1:(pop_size - n_elit))
     # new_pop = gen_n_children(pop_size - n_elit, selection_mechanism, perf, x_vec, weights)
     append!(new_pop, elit_pop)
-    global pop = new_pop
+    # global pop = new_pop
+    pop = new_pop
 end
 
 
