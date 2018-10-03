fcdd584670b2f5554f2beac4159508a55e454ee3
Gustav Karreskog <gustav.karreskog@gmail.com>
2018-10-03 07:28:53 -0400

    Added last iteration over population with a last number choices to calculate more exact fitness once evolutionary algorithm is completed. And some minor changes.

--- julia/main_linear_DT.jl
+++ julia/main_linear_DT.jl
@@ -3,9 +3,9 @@ push!(LOAD_PATH, pwd())
 
 using LinearDT
 using Distributed
-using SharedArrays
 using Statistics: mean
-## Setup for parallisation the @everywhere keyword is used to maked vairables, functions etc. available for all workers
+
+## Setup for parallisation, the @everywhere keyword is used to maked vairables, functions etc. available for all workers
 addprocs(Sys.CPU_THREADS)
 @everywhere push!(LOAD_PATH, pwd())
 @everywhere using LinearDT
@@ -13,30 +13,28 @@ addprocs(Sys.CPU_THREADS)
 # %%
 # All theses values have to be shared among the workers
 @everywhere begin
-    pop_size = 100
-    max_t = 3
+    pop_size = 200
+    max_t = 5
     p_extend = 0.7
-    n_elit = 2
-    periods = 5
+    n_elit = 3
+    periods = 10
     n_problems = 1000
-    sigmas = [1., 0.7, 0.2, 0.1]
+    sigmas = [1., 1., 1., 1.]
     selection_mechanism = "tournament"
     pop_fun = x -> init_tree(max_t, p_extend, sigmas)
 end
 
 # The globals are needed to work from terminal, bugish behavior
-# global pop = [init_tree(max_t, p_extend, sigmas) for i in 1:pop_size]
-# global prev_best = deepcopy(pop[1])
 global pop = [init_tree(max_t, p_extend, sigmas) for i in 1:pop_size]
 global prev_best = pop[1]
 
 
+
 for i in 1:periods
-    # The globals are needed to work from terminal, bugish behavior
     global pop = pop
     global prev_best = prev_best
-    println(prev_best in pop)
     x_vec = gen_investment_list(sigmas, n_problems)
+    prev_best_fit = fitness(prev_best, x_vec)
     @everywhere begin
         x_vec = $x_vec
         single_perf = tree -> (tree=tree, fit=fitness(tree, x_vec))
@@ -44,11 +42,10 @@ for i in 1:periods
     perf = pmap(single_perf, pop)
     # perf = gen_perf(pop, x_vec)
     sort!(perf, rev=true, by = x -> x.fit)
-    print(pop_size)
 
     println(string("----- Best individual in iteration: ", i, " -------"))
     println(string("Avg perf:", mean([x.fit for x in perf])))
-    println(string("Prevbest:", fitness(prev_best, x_vec)))
+    println(string("Prevbest:", prev_best_fit))
     println(string("Perf: ", perf[1].fit, "\n", perf[1].tree))
     if selection_mechanism == "rank"
         weights = [1/i for i in 1:pop_size]
@@ -59,7 +56,6 @@ for i in 1:periods
         weights = Float64[]
     end
     elit_pop = [x.tree for x in perf[1:n_elit]]
-    # global prev_best = elit_pop[1]
     prev_best = elit_pop[1]
     @everywhere begin
         weights = $weights
@@ -73,9 +69,22 @@ for i in 1:periods
     pop = new_pop
 end
 
+# %%
+
+# After the algorithm is done, compare all remaining trees on
+# a larger set of problems to find best.
+n_problems = 100000
+@time x_vec = gen_investment_list(sigmas, n_problems)
+@everywhere begin
+    x_vec = $x_vec
+    pop_fun = x -> init_tree(max_t, p_extend, sigmas)
+    single_perf = tree -> (tree=tree, fit=fitness(tree, x_vec))
+end
 
 
-
+@everywhere pop = $pop
+@time perf = pmap(single_perf, pop; batch_size=1)
+# sort!(perf, rev=true, by = x -> x.fit)
 
 ### Old non-parallised code
 # pop_size = 200
