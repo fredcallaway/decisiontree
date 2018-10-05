bf230736ff727c2933e407d66a253966211e4a43
Gustav Karreskog <gustav.karreskog@gmail.com>
2018-10-04 20:54:16 -0400

    Moved constants to named tuple params

--- julia/LinearDT.jl
+++ julia/LinearDT.jl
@@ -10,32 +10,32 @@ using StatsBase: sample, Weights
 # so we should probably change this at some point.
 
 # Constants for relevant parameters, constants compile to faster code.
-const feature_cost = 0.1
-const decision_cost = 0.01
-const p_crossover = 0.3
-const p_mutate_params = 0.7
-const p_mutate_w = 0.5
-const p_mutate_threshold = 0.5
-const p_mutate_end = 0.3
-const p_mutate_subtree = 0.3
-const p_opt_decisions = 0.5
-const p_opt_tree = 0.5
-const p_reduce = 0.5
-const p_trim = 0.5
-const tourn_size = 3
-const n_items = 2
+# const feature_cost = 0.05
+# const decision_cost = 0.01
+# const p_crossover = 0.3
+# const p_mutate_params = 0.7
+# const p_mutate_w = 0.5
+# const p_mutate_threshold = 0.5
+# const p_mutate_end = 0.3
+# const p_mutate_subtree = 0.3
+# const p_opt_decisions = 0.5
+# const p_opt_tree = 0.5
+# const p_reduce = 0.5
+# const p_trim = 0.5
+# const tourn_size = 3
+# const n_items = 2
 
 Investment = Vector{Vector{Float64}}
 
 "Generates a single investment problem"
-function gen_investment(sigmas::Vector{Float64})::Investment
+function gen_investment(sigmas::Vector{Float64}, n_items::Int64)::Investment
     x = [randn(length(sigmas)).*sigmas for i in 1:n_items]
     return x
 end
 
 "Generates a list of n choices to make"
-function gen_investment_list(sigmas::Vector{Float64}, n::Int64)::Vector{Investment}
-    [gen_investment(sigmas) for i in 1:n]
+function gen_investment_list(n::Int64, sigmas::Vector{Float64}, params)::Vector{Investment}
+    [gen_investment(sigmas, params.n_items) for i in 1:n]
 end
 
 "Generates a vector of the features of the two choices"
@@ -54,7 +54,7 @@ function rand_threshold()::Float64
 end
 
 "Generates a random decsion ∈ [1,2]"
-function rand_d()::Int64
+function rand_d(n_items::Int64)::Int64
     rand(1:n_items)
 end
 
@@ -66,12 +66,12 @@ mutable struct Node
     threshold::Float64
     left::Union{Int64,Node}
     right::Union{Int64,Node}
-    function Node(sigmas::Vector{Float64})
+    function Node(sigmas::Vector{Float64}, n_items::Int64)
         w_len = length(sigmas)*n_items
         w = [rand_w() for i in 1:w_len]
         threshold = rand_threshold()
-        left = rand_d()
-        right = rand_d()
+        left = rand_d(n_items)
+        right = rand_d(n_items)
         new(w_len, sigmas, w, threshold, left, right)
     end
 end
@@ -117,13 +117,13 @@ function dt_decide(node::Union{Node,Int64}, features::Vector{Float64}, cost_vec:
 end
 
 "Generates a tree of max length max_t, and exptends in each direction with probability p_extend"
-function init_tree(max_t::Int64, p_extend::Float64, sigmas::Vector{Float64})::Node
-    root = Node(sigmas)
+function init_tree(max_t::Int64, p_extend::Float64, sigmas::Vector{Float64}, params)::Node
+    root = Node(sigmas, params.n_items)
     if (max_t > 1) && (rand() < p_extend)
-        root.left = init_tree(max_t - 1, p_extend, sigmas)
+        root.left = init_tree(max_t - 1, p_extend, sigmas, params)
     end
     if (max_t > 1) && (rand() < p_extend)
-        root.right = init_tree(max_t - 1, p_extend, sigmas)
+        root.right = init_tree(max_t - 1, p_extend, sigmas, params)
     end
     return root
 end
@@ -141,25 +141,31 @@ function gen_node_list(node::Node)::Vector{Node}
 end
 
 "Generates a subtree, that can be just a decision. Used by mutate_subtree"
-function random_subtree(max_t::Int64, p_extend::Float64, sigmas::Vector{Float64})::Union{Node, Int64}
+function random_subtree(max_t::Int64, p_extend::Float64, sigmas::Vector{Float64}, params)::Union{Node, Int64}
     if rand() < 1/max_t
-        root = rand_d()
+        root = rand_d(params.n_items)
     else
-        root = init_tree(max_t, p_extend, sigmas)
+        root = init_tree(max_t, p_extend, sigmas, params)
     end
     return root
 end
 
 "Actual decision payoff, without regard to cognitive cost or trees"
 function decision_payoff(x::Investment, choice::Int64)::Float64
-    sum(x[choice])
+    # if x[choice][1] > 0
+    #     payoff = sum(x[choice])
+    # else
+    #     payoff = -sum(x[choice])
+    # end
+    payoff = sum(x[choice])
+    return payoff
 end
 
 "Actual best decision of problem, used for comparisons with tree decisions"
-function actual_best_decision(x::Investment)::Tuple{Int64, Float64}
+function actual_best_decision(x::Investment, params)::Tuple{Int64, Float64}
     best_payoff = decision_payoff(x, 1)
     best_choice = 1
-    for i in 2:n_items
+    for i in 2:params.n_items
         payoff = decision_payoff(x, i)
         if payoff > best_payoff
             best_payoff = payoff
@@ -171,7 +177,7 @@ end
 
 
 "Calcualte fitness for a tree from a single decision"
-function fitness_single(tree::Node, x::Investment)::Float64
+function fitness_single(tree::Node, x::Investment, feature_cost::Float64, decision_cost::Float64)::Float64
     features = gen_features(x)
     choice, cost_vec, n_decisions = dt_decide(tree, features)
     payoff = decision_payoff(x,choice)
@@ -180,12 +186,12 @@ function fitness_single(tree::Node, x::Investment)::Float64
 end
 
 "Calculates the fitness over a vector of decsions for a tree"
-function fitness(tree::Node, x_vec::Vector{Investment})::Float64
-    fitness = sum(fitness_single(tree,x) for x in x_vec)
+function fitness(tree::Node, x_vec::Vector{Investment}, feature_cost::Float64, decision_cost::Float64)::Float64
+    fitness = sum(fitness_single(tree,x, feature_cost, decision_cost) for x in x_vec)
 end
 
 "Calculates the crossover of two trees"
-function cross_over(tree1::Node, tree2::Node)::Node
+function cross_over(tree1::Node, tree2::Node, p_crossover::Float64)::Node
     new_tree = deepcopy(tree1)
     if rand() < p_crossover
         node = rand(gen_node_list(new_tree))
@@ -200,49 +206,49 @@ function cross_over(tree1::Node, tree2::Node)::Node
 end
 
 "Mutates the params of a tree"
-function mutate_params(tree::Node)
+function mutate_params(tree::Node, params)
     for node in gen_node_list(tree)
         for i in 1:node.w_len
-            if rand() < p_mutate_w
+            if rand() < params.p_mutate_w
                 node.w[i] = rand_w()
             end
         end
-        if rand() < p_mutate_threshold
+        if rand() < params.p_mutate_threshold
             node.threshold = rand_threshold()
         end
 
-        if isa(node.left, Int64) && (rand() < p_mutate_end)
-            node.left = rand_d()
+        if isa(node.left, Int64) && (rand() < params.p_mutate_end)
+            node.left = rand_d(params.n_items)
         end
-        if isa(node.right, Int64) && (rand() < p_mutate_end)
-            node.right = rand_d()
+        if isa(node.right, Int64) && (rand() < params.p_mutate_end)
+            node.right = rand_d(params.n_items)
         end
     end
 end
 
 "Replaces a node/leaf with a random subtree"
-function mutate_subtree(tree::Node, max_t::Int64=2, p_extend::Float64=0.5)
+function mutate_subtree(tree::Node, params)
     node = rand(gen_node_list(tree))
     if rand() < 0.5
-        node.left = random_subtree(max_t, p_extend, node.sigmas)
+        node.left = random_subtree(params.max_t - 1, params.p_extend, node.sigmas, params)
     else
-        node.right = random_subtree(max_t, p_extend, node.sigmas)
+        node.right = random_subtree(params.max_t - 1, params.p_extend, node.sigmas, params)
     end
 end
 
 "Makes a local optimazation for all decision nodes"
-function opt_decisions(tree::Node, x_vec::Vector{Investment})
+function opt_decisions(tree::Node, x_vec::Vector{Investment}, params)
     for node in gen_node_list(tree)
         for branch in (:left, :right)
             child = getfield(node, branch)
             if isa(child, Int64)
                 best_child = child
-                best_fitness = fitness(tree, x_vec)
-                test_vals = collect(1:n_items)
+                best_fitness = fitness(tree, x_vec, params.feature_cost, params.decision_cost)
+                test_vals = collect(1:params.n_items)
                 filter!(x -> x != best_child, test_vals)
                 for val in test_vals
                     setfield!(node, branch, val)
-                    fit = fitness(tree, x_vec)
+                    fit = fitness(tree, x_vec, params.feature_cost, params.decision_cost)
                     if fit > best_fitness
                         best_fitness = fit
                         best_child = val
@@ -256,15 +262,15 @@ end
 
 
 "Makes a local optimazation for all params, w and threshold, for a node"
-function opt_node_params(tree::Node, node::Node, x_vec::Vector{Investment})
-    best_fitness = fitness(tree, x_vec)
+function opt_node_params(tree::Node, node::Node, x_vec::Vector{Investment}, params)
+    best_fitness = fitness(tree, x_vec, params.feature_cost, params.decision_cost)
     for i in 1:node.w_len
         best_val = node.w[i]
         test_vals = [-1,0,1]
         filter!(x -> x != best_val, test_vals)
         for val in test_vals
             node.w[i] = val
-            fit = fitness(tree, x_vec)
+            fit = fitness(tree, x_vec, params.feature_cost, params.decision_cost)
             if fit > best_fitness
                 best_val = val
                 best_fitness = fit
@@ -276,7 +282,7 @@ function opt_node_params(tree::Node, node::Node, x_vec::Vector{Investment})
     best_threshold = node.threshold
     for a in range(-1.5,stop=1.5, length=19)
         node.threshold = a
-        fit = fitness(tree, x_vec)
+        fit = fitness(tree, x_vec, params.feature_cost, params.decision_cost)
         if fit > best_fitness
             best_fitness = fit
             best_threshold = a
@@ -286,23 +292,23 @@ function opt_node_params(tree::Node, node::Node, x_vec::Vector{Investment})
 end
 
 "Iterates over all decision nodes in a tree and makes a local optimazation of parameters"
-function opt_tree_params(tree::Node, x_vec::Vector{Investment})
+function opt_tree_params(tree::Node, x_vec::Vector{Investment}, params)
     for node in gen_node_list(tree)
-        opt_node_params(tree, node, x_vec)
+        opt_node_params(tree, node, x_vec, params)
     end
 end
 
 "Iteratively replaces the nodes with a decision if that improves the fitness"
-function reduce_tree(tree::Node, x_vec::Vector{Investment})
+function reduce_tree(tree::Node, x_vec::Vector{Investment}, params)
     for node in gen_node_list(tree)
         for branch in (:left, :right)
             child = getfield(node, branch)
             if isa(child, Node)
                 best_node = child
-                best_fitness = fitness(tree, x_vec)
-                for decision in 1:n_items
+                best_fitness = fitness(tree, x_vec, params.feature_cost, params.decision_cost)
+                for decision in 1:params.n_items
                     setfield!(node, branch, decision)
-                    fit = fitness(tree, x_vec)
+                    fit = fitness(tree, x_vec, params.feature_cost, params.decision_cost)
                     if fit >= best_fitness
                         best_fitness = fit
                         best_node = decision
@@ -340,23 +346,23 @@ function trim_tree(tree::Node)
         end
     end
 end
-#
-"Generates a named tuple with tree at fitness for all trees in population"
-function gen_perf(pop::Vector{Node}, x_vec::Vector{Investment})::Vector{NamedTuple{(:tree, :fit),Tuple{Node, Float64}}}
-    [(tree=tree, fit=fitness(tree,x_vec)) for tree in pop]
-end
+# Currently not used
+# "Generates a named tuple with tree at fitness for all trees in population"
+# function gen_perf(pop::Vector{Node}, x_vec::Vector{Investment}, params)::Vector{NamedTuple{(:tree, :fit),Tuple{Node, Float64}}}
+#     [(tree=tree, fit=fitness(tree,x_vec)) for tree in pop]
+# end
 
 "Returns a single tournament winner"
-function sample_tourn_winner(perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Float64}},1})
+function sample_tourn_winner(perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Float64}},1}, tourn_size::Int64)
     tourn_set = rand(perf, tourn_size)
     sort!(tourn_set, rev=true, by = x -> x.fit)
     return tourn_set[1].tree
 end
 
 "Selects two parents by tournament selection"
-function tournament_selection(perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Float64}},1})
-    tree1 = sample_tourn_winner(perf)
-    tree2 = sample_tourn_winner(perf)
+function tournament_selection(perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Float64}},1}, tourn_size::Int64)
+    tree1 = sample_tourn_winner(perf, tourn_size)
+    tree2 = sample_tourn_winner(perf, tourn_size)
     return (tree1, tree2)
 end
 
@@ -368,31 +374,31 @@ function weighted_selection(perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Floa
 end
 
 "Selects two parents, creates a child with crossower, mutates, optimizes and trims"
-function gen_child(selection_mechanism::String, perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Float64}},1}, x_vec::Vector{Investment}, weights::Vector{Float64})::Node
+function gen_child(selection_mechanism::String, perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Float64}},1}, x_vec::Vector{Investment}, weights::Vector{Float64}, params)::Node
     if selection_mechanism == "tournament"
-        tree1, tree2 = tournament_selection(perf)
+        tree1, tree2 = tournament_selection(perf, params.tourn_size)
     else
         tree1, tree2 = weighted_selection(perf, weights)
     end
 
-    new_tree = cross_over(tree1, tree2)
+    new_tree = cross_over(tree1, tree2, params.p_crossover)
 
-    if rand() < p_mutate_params
-        mutate_params(new_tree)
+    if rand() < params.p_mutate_params
+        mutate_params(new_tree, params)
     end
-    if rand() < p_mutate_subtree
-        mutate_subtree(new_tree)
+    if rand() < params.p_mutate_subtree
+        mutate_subtree(new_tree, params)
     end
-    if rand() < p_opt_decisions
-        opt_decisions(new_tree, x_vec)
+    if rand() < params.p_opt_decisions
+        opt_decisions(new_tree, x_vec, params)
     end
-    if rand() < p_opt_tree
-        opt_tree_params(new_tree, x_vec)
+    if rand() < params.p_opt_tree
+        opt_tree_params(new_tree, x_vec, params)
     end
-    if rand() < p_reduce
-        reduce_tree(new_tree, x_vec)
+    if rand() < params.p_reduce
+        reduce_tree(new_tree, x_vec, params)
     end
-    if rand() < p_trim
+    if rand() < params.p_trim
         trim_tree(new_tree)
     end
 
@@ -400,8 +406,8 @@ function gen_child(selection_mechanism::String, perf::Array{NamedTuple{(:tree, :
 end
 
 "Iteratively generates children unitl n are generated. Not currently in use with par"
-function gen_n_children(n::Int64, selection_mechanism::String, perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Float64}},1}, x_vec::Vector{Investment}, weights::Vector{Float64})::Vector{Node}
-    children = [gen_child(selection_mechanism, perf, x_vec, weights) for i in 1:n]
+function gen_n_children(n::Int64, selection_mechanism::String, perf::Array{NamedTuple{(:tree, :fit),Tuple{Node,Float64}},1}, x_vec::Vector{Investment}, weights::Vector{Float64}, params)::Vector{Node}
+    children = [gen_child(selection_mechanism, perf, x_vec, weights, params) for i in 1:n]
     return children
 end
 
--- julia/main_linear_DT.jl
+++ julia/main_linear_DT.jl
@@ -4,11 +4,13 @@ push!(LOAD_PATH, pwd())
 using LinearDT
 using Distributed
 using Statistics: mean
-
+using Dates
 ## Setup for parallisation, the @everywhere keyword is used to maked vairables, functions etc. available for all workers
 addprocs(Sys.CPU_THREADS)
+# addprocs(4)
 @everywhere push!(LOAD_PATH, pwd())
 @everywhere using LinearDT
+
 
 # %%
 # All theses values have to be shared among the workers
@@ -17,27 +19,29 @@ addprocs(Sys.CPU_THREADS)
     max_t = 5
     p_extend = 0.7
     n_elit = 3
-    periods = 10
+    periods = 20
     n_problems = 10000
-    sigmas = [1., 0.5, 0.2, 0.1]
+    sigmas = [1., 0.7, 0.5, 0.2]
     selection_mechanism = "tournament"
+    params = (pop_size=200, n_items=3, max_t=5, p_extend=0.7, sigmas=sigmas, selection_mechanism=selection_mechanism, feature_cost=0.05, decision_cost=0.01, p_crossover=0.3, p_mutate_params=0.7, p_mutate_w=0.5, p_mutate_threshold=0.5, p_mutate_end=0.3, p_mutate_subtree=0.3, p_opt_decisions=0.5, p_opt_tree=0.5, p_reduce=0.5, p_trim=0.5, tourn_size=3)
     pop_fun = x -> init_tree(max_t, p_extend, sigmas)
 end
 
 # The globals are needed to work from terminal, bugish behavior
-global pop = [init_tree(max_t, p_extend, sigmas) for i in 1:pop_size]
+global pop = [init_tree(max_t, p_extend, sigmas, params) for i in 1:pop_size]
 global prev_best = pop[1]
 
 
 
 for i in 1:periods
+    println(Dates.format(now(), "HH:MM:SS"))
     global pop = pop
     global prev_best = prev_best
-    x_vec = gen_investment_list(sigmas, n_problems)
-    prev_best_fit = fitness(prev_best, x_vec)
+    x_vec = gen_investment_list(n_problems, sigmas, params)
+    prev_best_fit = fitness(prev_best, x_vec, params.feature_cost, params.decision_cost)
     @everywhere begin
         x_vec = $x_vec
-        single_perf = tree -> (tree=tree, fit=fitness(tree, x_vec))
+        single_perf = tree -> (tree=tree, fit=fitness(tree, x_vec, params.feature_cost, params.decision_cost))
     end
     perf = pmap(single_perf, pop)
     # perf = gen_perf(pop, x_vec)
@@ -60,7 +64,7 @@ for i in 1:periods
     @everywhere begin
         weights = $weights
         perf = $perf
-        gen_child_parallel = x-> gen_child(selection_mechanism, perf, x_vec, weights)
+        gen_child_parallel = x-> gen_child(selection_mechanism, perf, x_vec, weights, params)
     end
     new_pop = pmap(gen_child_parallel, 1:(pop_size - n_elit))
     # new_pop = gen_n_children(pop_size - n_elit, selection_mechanism, perf, x_vec, weights)
@@ -74,18 +78,18 @@ end
 # After the algorithm is done, compare all remaining trees on
 # a larger set of problems to find best.
 n_problems = 100000
-@time x_vec = gen_investment_list(sigmas, n_problems)
+x_vec = gen_investment_list(n_problems, sigmas, params)
 @everywhere begin
     x_vec = $x_vec
     pop_fun = x -> init_tree(max_t, p_extend, sigmas)
-    single_perf = tree -> (tree=tree, fit=fitness(tree, x_vec))
+    single_perf = tree -> (tree=tree, fit=fitness(tree, x_vec, params.feature_cost, params.decision_cost))
 end
 
 
 @everywhere pop = $pop
 @time perf = pmap(single_perf, pop)
 sort!(perf, rev=true, by = x -> x.fit)
-best_tree = perf[1].tree
+best_tree = perf[1].tree;
 
 
 # %%
@@ -96,11 +100,13 @@ get_decision_payoff_tuple = x -> begin
     (decision, decision_payoff(x, decision))
 end
 tree_decisions = [get_decision_payoff_tuple(x) for x in x_vec]
-opt_decisions = [actual_best_decision(x) for x in x_vec]
+opt_decisions = [actual_best_decision(x, params) for x in x_vec];
 
-#%%
-println(string("Avg payoff from tree decisions:", mean([x[2] for x in tree_decisions])))
-println(string("Avg payoff from tree decisions:", mean([x[2] for x in opt_decisions])))
+avg_tree_profit = mean([x[2] for x in tree_decisions])
+avg_opt_profit = mean([x[2] for x in opt_decisions])
+println(string("Avg payoff from tree decisions:", avg_tree_profit))
+println(string("Avg payoff from tree decisions:", avg_opt_profit))
+println(string("Share of profit: ", avg_tree_profit/avg_opt_profit))
 println(string("Share same decision:", sum([x[1] for x in opt_decisions] .== [x[1] for x in tree_decisions])/length(tree_decisions)))
 
 
@@ -121,7 +127,7 @@ println(string("Share same decision:", sum([x[1] for x in opt_decisions] .== [x[
 # pop = [init_tree(max_t, p_extend, sigmas) for i in 1:pop_size]
 # prev_best = pop[1]
 # for i in 1:periods
-#     x_vec = gen_investment_list(sigmas, n_problems)
+#     x_vec = gen_investment_list(n_problems, sigmas)
 #     perf = gen_perf(pop, x_vec)
 #     sort!(perf, rev=true, by = x -> x[2])
 #     println(string("----- Best individual in iteration: ", i, " -------"))
